bcbadbeb8e987982bdf6e9cf821f2c389d1f613b
Diony Rosa <dhrosa@gmail.com>
2024-01-10 14:35:00 -0500

    Simple POC for repeated A button presses.

---
+++ src/buttons.py
@@ -0,0 +1,33 @@
+Y = 0
+B = 1
+A = 2
+X = 3
+L = 4
+R = 5
+ZL = 6
+ZR = 7
+Minus = 8
+Plus = 9
+LS = 10
+RS = 11
+
+class Buttons:
+    def __init__(self):
+        self.data = 0
+
+    def __getitem__(self, index):
+        mask = 1 << index
+        return bool(self.data & mask)
+
+    def __setitem__(self, index, value):
+        mask = 1 << index
+        if value:
+            self.data |= mask
+        else:
+            self.data &= ~mask
+
+    def __str__(self):
+        return f'{self.data:2X}'
+
+    def __bytes__(self):
+        return self.data.to_bytes(2, 'little')
--- src/code.py
+++ src/code.py
@@ -1,15 +1,17 @@
 import usb_hid
 import time
 import board
-from neopixel_write import neopixel_write
 import digitalio
+from adafruit_macropad import MacroPad
 
+from buttons import *
+
+print("\n")
+
+macropad = MacroPad()
 gamepad = usb_hid.devices[0]
 
 REPORT_LENGTH = 9
-
-led_pin = digitalio.DigitalInOut(board.NEOPIXEL)
-led_pin.switch_to_output()
 
 def led_colors():
     while True:
@@ -22,14 +24,29 @@ def led_colors():
 
 
 color = led_colors()
+buttons = Buttons()
+active = False
 while True:
-    for byte_index in range(REPORT_LENGTH):
-        for bit_index in range(8):
-            report = bytearray(REPORT_LENGTH)
-            report[byte_index] = (1 << bit_index)
-            print(f"Sending report: {report.hex(' ')}")
-            neopixel_write(led_pin, bytes(next(color)))
-            try:
-                gamepad.send_report(report)
-            except OSError:
-                print("Skipping due to OS error.")
+    timescale = 1_000_000_000
+    t = (time.monotonic_ns() % timescale) / timescale
+    event = macropad.keys.events.get()
+    if event:
+        if event.pressed:
+            active = not active
+            print(f"{active=}")
+        if active:
+            macropad.pixels.fill(next(color))
+        else:
+            macropad.pixels.fill((0, 0, 0))
+    buttons[A] = active * (t < 0.25)
+    print(buttons)
+
+    report = bytearray(REPORT_LENGTH)
+    report[0:2] = bytes(buttons)
+    #print(t)
+    #print(buttons)
+    #print(f"Sending report: {report.hex(' ')}")
+    try:
+        gamepad.send_report(report)
+    except OSError:
+        print("Skipping due to OS error.")
