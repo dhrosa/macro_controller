6a2af08ebb69eaaadea71e27d9a1e6c7852a85bc
Diony Rosa <dhrosa@gmail.com>
2024-01-12 22:43:13 -0500

    Split enums into separate file

--- src/code.py
+++ src/code.py
@@ -12,53 +12,12 @@ import usb_hid
 from adafruit_macropad import MacroPad
 
 from bitfield import Bitfield
+from enums import buttons, keys, hat
 
 print("\n")
 
 macropad = MacroPad()
 gamepad = usb_hid.devices[0]
-
-
-class buttons:
-    Y = 0
-    B = 1
-    A = 2
-    X = 3
-    L = 4
-    R = 5
-    ZL = 6
-    ZR = 7
-    MINUS = 8
-    PLUS = 9
-    LS = 10
-    RS = 11
-
-
-class hat:
-    N = 0
-    NE = 1
-    E = 2
-    SE = 3
-    S = 4
-    SW = 5
-    W = 6
-    NW = 7
-    CENTER = 8
-
-
-class keys:
-    L = 0
-    UP = 1
-    R = 2
-    LEFT = 3
-    PULSE = 4
-    RIGHT = 5
-    Y = 6
-    DOWN = 7
-    X = 8
-    B = 9
-    PLUS = 10
-    A = 11
 
 
 def map_analog_value(value: float) -> int:
